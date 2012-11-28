256f8ba2b27a4296c4a00fddce3b119dca507b7e
AndrewSC <hello@binarygeometry.net>
2012-11-28 02:47:25 +0000

    update time on click added

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,21 +1,19 @@
 $(document).ready(function() {
 
-	$(".edit").editable("http://localhost/waitingfortheworldtoend/echo.php");
+	
+
+	 $(".edit").editable("echo.php", {
+		 placeholder : "Click me to edit",
+		 loadtype : "POST",
+		 callback: function(value, settings) {
+		  startTime = new Date().getTime();
+		  console.log(startTime);
+		 }
+	 });
+
 	
 	$("#done").click(function(e){
-		$(".taskList ul li").appendTo(".doneList")
-		$(".edit").removeClass("edit").unbind("click.editable");
-		$("<li><div class='nine columns task'><div class='edit'>..I hope I can bill for this</div><div class='three columns time'><p>19:35</p></div></li>")
-			.appendTo(".taskList ul");
-		$(".edit").editable("http://localhost/waitingfortheworldtoend/echo.php");
-		e.preventDefault();
-	});
-
-
 	
-	
-		startTime = new Date().getTime();
-		
 		function update_output() {
 		
 			
@@ -25,23 +23,37 @@ $(document).ready(function() {
 				
 			for (var i = 0, len = clicks.length; i < len; i++) {
 				displayTime = (clicks[i].time - startTime);
-				//console.log(displayTime);
+				console.log(displayTime);
 				outputTime = displayTime //* 0.00001666666;
-				//console.log(outputTime);
+				console.log(outputTime);
 				outputTime = outputTime.toFixed(0);
 				console.log(outputTime);
 				out.push('<li>' + clicks[i].target + ' (' + outputTime + 's)</li>');
 			}
-			$('#output').html(out.join(''));
+			$(".taskList ul li p").html(out.join(''));
 		}
 		
 		var clicks = [];
-    
-    $('#done').on('click', function () {
+		
         clicks.push({ time : new Date().getTime(), target : $(this).attr('href') });
         update_output();
-    });
-
-    
+		
+		
+		$(".taskList ul li").appendTo(".doneList")
+		$(".edit").removeClass("edit").unbind("click.editable");
+		$("<li><div class='nine columns task'><div class='edit'>..click to edit</div><div class='three columns time'><p></p></div></li>")
+			.appendTo(".taskList ul");
+		
+		$(".edit").editable("echo.php", {
+			 placeholder : "Click me to edit",
+			 loadtype : "POST",
+			 callback: function(value, settings) {
+				startTime = new Date().getTime();
+				console.log(startTime);
+			 }
+		 });
+		e.preventDefault();//
+	});
+  
 	
 });
